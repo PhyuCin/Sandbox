c81ce41b663abcab1b291791734d5862927dc09d
Aye Mya Phyu Cin <ayemyaphyucin@my.jcu.edu.au>
2018-11-20 14:08:58 +0800

    Add main function and moved all the codes into it

--- oddname.py
+++ oddname.py
@@ -1,9 +1,14 @@
 """Input a name and print alternate character"""
 
-name = input("Enter name: ")
 
-while len(name) <= 1:
-    print("Invalid")
-    name = input("Enter long name: ")
+def main():
+    name = input("Enter name: ")
 
-print(name[::2])
+    while len(name) <= 1:
+        print("Invalid")
+        name = input("Enter long name: ")
+
+    print(name[::2])
+
+
+main()
